36b7b780537f2104110f5d68880432fd8a22101d
Mihailo Gostiljac <91563699+DeyolGit@users.noreply.github.com>
2026-08-30 19:53:29 +0200

    Add files via upload

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,6 @@
-oh-my-posh init fish --config $HOME/.poshthemes/agnoster.omp.json | source
+oh-my-posh init fish --config $HOME/.config/fish/completions/amro.omp.json | source
 set -g fish_greeting ""
-fastfetch --logo Arch
+fastfetch --logo-type kitty-direct --logo ~/tux.png --logo-width 33 --logo-height 15
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
